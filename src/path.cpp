f7ae5917bb08ee40e6e6546d16812243cb05cc1b
Fredrik Sævland <fredrik.savland@laerdal.com>
2023-11-17 20:24:59 +0100

    Preliminary implementation of A*

--- src/path.cpp
+++ src/path.cpp
@@ -8,15 +8,18 @@
 
 #include "path.h"
 #include "map.h"
+#include <cmath>
+
 
 Path::Path(WorldMap& map) : map(map) {
     // Constructor
 }
 
-bool Path::isValid(int x, int y) { //If our Node is an obstacle it is not valid
-    int id = x + y * (X_MAX / X_STEP);
-    if (world.obstacles.count(id) == 0) {
-        if (x < 0 || y < 0 || x >= (X_MAX / X_STEP) || y >= (Y_MAX / Y_STEP)) {
+bool Path::is_valid(std::pair<uint32_t, uint32_t> pos) { //If our Node is an obstacle it is not valid
+    //uint32_t id = pos.first + pos.second * (x_max / x_step);
+    if (map.get_cell(pos) == WorldMap::CellType::EMPTY) {
+        if (pos.first < 0 || pos.second < 0 
+            || pos.first >= (x_max / x_step) || pos.second >= (y_max / y_step)) {
             return false;
         }
         return true;
@@ -24,3 +27,30 @@ bool Path::isValid(int x, int y) { //If our Node is an obstacle it is not valid
     return false;
 }
 
+bool Path::is_destination(std::pair<uint32_t, uint32_t> pos, Node dest) {
+    if (pos.first == dest.x && pos.second == dest.y) {
+        return true;
+    }
+    return false;
+}
+
+float Path::calculate_h(std::pair<uint32_t, uint32_t> pos, Node dest) {
+    // Return using the distance formula
+    return static_cast<float>(sqrt((pos.first - dest.x) * (pos.first - dest.x)
+        + (pos.second - dest.y) * (pos.second - dest.y)));
+}
+
+std::vector<Node> Path::a_star(Node robot, Node target) {
+    std::vector<Node> empty;
+    if(is_valid(std::make_pair(target.x, target.y)) == false) {
+        //Destination is invalid
+        return empty;
+    }
+
+    if(is_destination(std::make_pair(robot.x, robot.y), target) == true) {
+        //You clicked on yourself
+        return empty;
+    }
+
+    std::array<std::array<bool, y_max / y_step>, x_max / x_step> closed_list;
+}
--- src/path.h
+++ src/path.h
@@ -2,6 +2,7 @@
 #ifndef PATH_H
 #define PATH_H
 
+#include <vector>
 #include "map.h"
 
 struct Node
@@ -14,6 +15,11 @@ struct Node
     float h_cost;
     float f_cost;
 };
+
+inline bool operator < (const Node& lhs, const Node& rhs)
+{//We need to overload "<" to put our struct into a set
+	return lhs.f_cost < rhs.f_cost;
+}
 
 class Path {
 public:
@@ -28,15 +34,25 @@ public:
 
 private:
     WorldMap& map;
+
     std::pair<uint32_t, uint32_t> from;
     std::pair<uint32_t, uint32_t> to;
 
     static constexpr uint32_t x_max = WorldMap::map_dim;
-    static constexpr uint32_t x_step = 10;
+    static constexpr uint32_t x_step = 4;
     static constexpr uint32_t y_max = WorldMap::map_dim;
-    static constexpr uint32_t y_step = 10;
+    static constexpr uint32_t y_step = 4;
+
+    std::array<std::array<Node, y_max / y_step>, x_max / x_step> node_map; //Scaled node map
     //std::array<std::array<WorldMap::CellType, WorldMap::map_dim>, WorldMap::map_dim> search_map;
     // Member variables
+
+    bool is_valid(std::pair<uint32_t, uint32_t> pos);
+    bool is_destination(std::pair<uint32_t, uint32_t> pos, Node dest);
+
+    float calculate_h(std::pair<uint32_t, uint32_t> pos, Node dest);
+
+    std::vector<Node> a_star(Node robot, Node target);
 
 };
 
